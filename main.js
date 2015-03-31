a5992b15dba03a615d9ab770be881a9725403437
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-03-31 19:16:55 -0400

    pushing jquery

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$('.menu-cont').on('click', function(event) {
+	event.preventDefault();
+  
+  var open = $('.menu-txt').text();
+	var close = $('.menu-txt').attr('data-text');
+  
+  $('.menu').toggleClass('active');
+  
+  if ($('.menu-txt').text(open)) {
+  	$('.menu-txt').text(close);
+  	$('#sidebarToggle')[0].checked = true;
+  } else {
+  	$('#sidebarToggle')[0].checked = false;
+    $('.menu-txt').text(open);
+
+
+  } 
+  	$('.menu-txt').attr('data-text',open);
+	});
+});
