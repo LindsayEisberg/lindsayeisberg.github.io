9837a800854d535de1cf092ad6b7ec0f34a82a93
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-04-01 15:14:52 -0400

    laying out all pages

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
-$('.menu-cont').on('click', function(event) {
-	event.preventDefault();
+// $('.menu-cont').on('click', function(event) {
+//   event.preventDefault();
   
-  var open = $('.menu-txt').text();
-	var close = $('.menu-txt').attr('data-text');
+//   var open = $('.menu-txt').text();
+//   var close = $('.menu-txt').attr('data-text');
   
-  $('.menu').toggleClass('active');
+//   $('.menu').toggleClass('active');
   
-  if ($('.menu-txt').text(open)) {
-  	$('.menu-txt').text(close);
-  	$('#sidebarToggle')[0].checked = true;
-  } else {
-  	$('#sidebarToggle')[0].checked = false;
-    $('.menu-txt').text(open);
+//   if ($('.menu-txt').text(open)) {
+//     $('.menu-txt').text(close);
+//     $('.sidebarToggle')[0].checked = true;
+//   } else {
+//    $('.sidebarToggle')[0].checked = false;
+//     $('.menu-txt').text(open);
 
-  } 
-  	$('.menu-txt').attr('data-text',open);
-	});
+
+//   } 
+//     $('.menu-txt').attr('data-text',open);
+
+
+//   });
+  
+  
 
       $('#fullpage').fullpage({
-
-
-      
-    });
+      });
 
 });
