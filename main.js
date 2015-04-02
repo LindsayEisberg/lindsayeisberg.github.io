7612c32fa29667b336c2234e6cb1feb0c39e4f89
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-04-01 23:35:01 -0400

    work page

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
 $(document).ready(function(){
-// $('.menu-cont').on('click', function(event) {
-//   event.preventDefault();
-  
-//   var open = $('.menu-txt').text();
-//   var close = $('.menu-txt').attr('data-text');
-  
-//   $('.menu').toggleClass('active');
-  
-//   if ($('.menu-txt').text(open)) {
-//     $('.menu-txt').text(close);
-//     $('.sidebarToggle')[0].checked = true;
-//   } else {
-//    $('.sidebarToggle')[0].checked = false;
-//     $('.menu-txt').text(open);
+  var menuOpen = false;
 
 
-//   } 
-//     $('.menu-txt').attr('data-text',open);
+$('.menu-cont').on('click', function(event) {
+  event.preventDefault();
+  
+  var open = $('.menu-txt').text();
+  var close = $('.menu-txt').attr('data-text');
+
+  $('.menu').toggleClass('active');
+  
+  if (!menuOpen) {
+    $('.menu-txt').text(close);
+    $('#sidebarToggle')[0].checked = true;
+    menuOpen = true;
+    console.log(menuOpen)
+  } 
+  else if (menuOpen) {
+   $('#sidebarToggle')[0].checked = false;
+    $('.menu-txt').text('open');
+    menuOpen = false;
+  } 
+    $('.menu-txt').attr('data-text',open);
 
 
-//   });
-  
-  
+  });
+        $('.your-class').slick({
+    dots: true,
+    draggable: false,
+    arrows: true
+  });
 
       $('#fullpage').fullpage({
       });
+
+
+  // $(document).ready(function(){
+  //   $('.your-class').slick({
+  //      setting-name: setting-value
+  //   });
+  // });
+        
 
 });
